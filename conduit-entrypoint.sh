7bb8a32b092bd765f8f9e46da4e2c0d5303af7cb
chanchilun2004 <chanchilun2004@gmail.com>
2026-05-19 17:48:49 +0800

    Update conduit-entrypoint.sh

--- conduit-entrypoint.sh
+++ conduit-entrypoint.sh
@@ -7,4 +7,5 @@ if [ -n "$META_REGISTRATION_B64" ]; then
   export CONDUIT_APPSERVICE_CONFIG_FILES=/var/lib/matrix-conduit/meta-registration.yaml
 fi
 
-exec /nix/store/kmxkixnvfgwlymj8d2r3h093wlklxc6v-conduit-0.10.12/bin/conduit "$@"
+echo "[entrypoint] Starting conduit..."
+exec /bin/conduit "$@"
