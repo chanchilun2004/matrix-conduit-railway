09d885b6e1b8b023b0217cc390eaaa40f1b79363
chanchilun2004 <chanchilun2004@gmail.com>
2026-05-19 17:37:42 +0800

    Update conduit-entrypoint.sh

--- conduit-entrypoint.sh
+++ conduit-entrypoint.sh
@@ -2,15 +2,11 @@
 set -e
 
 if [ -n "$META_REGISTRATION_B64" ]; then
-  /busybox mkdir -p /var/lib/matrix-conduit
-  /busybox echo "$META_REGISTRATION_B64" | /busybox base64 -d > /var/lib/matrix-conduit/meta-registration.yaml
+  /bin/mkdir -p /var/lib/matrix-conduit
+  /bin/echo "$META_REGISTRATION_B64" | /bin/base64 -d > /var/lib/matrix-conduit/meta-registration.yaml
   export CONDUIT_APPSERVICE_CONFIG_FILES=/var/lib/matrix-conduit/meta-registration.yaml
 fi
 
-echo "=== /nix ==="
-/busybox ls /nix 2>/dev/null || echo "no /nix"
-echo "=== /nix/var/nix/profiles ==="
-/busybox ls /nix/var/nix/profiles 2>/dev/null || echo "not found"
-echo "=== bin ==="
-/busybox ls /bin 2>/dev/null || echo "no /bin"
+echo "=== /nix/store ==="
+/busybox ls /nix/store/ 2>/dev/null | /busybox grep conduit
 exit 1
